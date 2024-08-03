cf9cdd464caeba38e52b37b813bca3f95d889e18
Ana Luiza Favacho <analufavacho@gmail.com>
2024-08-03 05:34:09 -0300

    add framer motion

--- onion-client/src/components/Banner/Banner.jsx
+++ onion-client/src/components/Banner/Banner.jsx
@@ -1,14 +1,21 @@
 import banner from "../../assets/bannerimage.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const Banner = () => {
     return (
         <div className="gradientBg rounded-xl rounded-br-[80px] md:p-9 px-4 py-9">
             <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-10">
-                <div>
+                <motion.div
+                    variants={fadeIn("down", 0.2)}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.7 }}
+                >
                     <img src={banner} className="lg:h-[386px]" />
-                </div>
+                </motion.div>
                 <div className="md:w-3/5">
-                    <h2 className="md:text-5xl text-4xl font-bold text-white mb-6 leaning-relaxed">
+                    <h2 className="md:text-4xl text-4xl font-bold text-white mb-6 leaning-relaxed">
                         Otimize suas operações de Vendas e Logística
                     </h2>
                     <p className="text-white text-xl mb-8">
